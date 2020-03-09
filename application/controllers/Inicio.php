c54540b202645ac9a8f48c62830eba2e251cbda9
lmvillegas <luism.villegass@gmail.com>
2020-03-09 12:33:50 -0400

    Vista Inicial

---
+++ application/controllers/Inicio.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * Controlador que permite gestionar los registro de usuarios del sistema
+ *
+ * @package         SGI
+ * @subpackage      Inicio
+ * @category        Controlador
+ * @author          Jcramos
+ * @author          Juan Carlos Ramos
+ * @link            http://sgi.sti.com.ve/
+ * @version         Current v0.1.0
+ * @copyright       Copyright (c) 2017 SGI
+ * @license         MIT
+ * @since           31/06/2017
+ */
+class Inicio extends CI_Controller {
+	public function __construct() {
+		parent::__construct();
+	/*	$ruta = 'dashboard/';
+		$this->load->model($ruta . 'Solicitudes_model');
+                $this->load->model($ruta . 'Computadoras_model');
+*/
+	}
+
+	public function index() {
+		$data = array(
+			'titulo' => 'Inicio ',
+			'contenido' => 'inicio',
+
+		);
+
+		$this->load->view(THEME . TEMPLATE, $data);
+	}
+
+}
